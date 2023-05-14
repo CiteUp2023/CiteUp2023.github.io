d1b5124ba3b9db2e19c93085b68a6173ec0f2ff6
farhandwi <farhandwi234@gmail.com>
2023-05-14 22:41:23 +0700

    14 Mei 2023

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-const day = document.querySelector('.day p')
-const hour = document.querySelector('.hour p')
-const minute = document.querySelector('.minute p')
-const second = document.querySelector('.second p')
+const day = document.querySelector(".day p");
+const hour = document.querySelector(".hour p");
+const minute = document.querySelector(".minute p");
+const second = document.querySelector(".second p");
 
-let countDownDate = new Date("May 15, 2023 00:00:00").getTime()
+let countDownDate = new Date("May 29, 2023 00:00:00").getTime();
 
 let x = setInterval(() => {
+  let now = new Date().getTime();
 
-  let now = new Date().getTime()
-    
-  let distance = countDownDate - now
-    
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24))
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000)
-  
-  day.innerHTML = days
-  hour.innerHTML = hours
-  minute.innerHTML = minutes
-  second.innerHTML = seconds
-}, 1000)
+  let distance = countDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  day.innerHTML = days;
+  hour.innerHTML = hours;
+  minute.innerHTML = minutes;
+  second.innerHTML = seconds;
+}, 1000);
